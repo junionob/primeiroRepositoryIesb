e99ecc35d0869d2289a4b104ac2fa75e7c095301
Waldomiro <junio.waldomiro@gmail.com>
2025-02-26 21:39:43 -0300

    aula 1, estrutura básica de um programa C

---
+++ aulas/aula01/ola_mundo.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //Incluindo a biblioteca principal
+
+//função principal que inicia o programa
+/* Aula 1, estrutura básica de um programa C
+ *
+ */
+int main(){
+    printf("Olá mundo!\n");
+
+    return 0;
+}
